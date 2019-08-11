75ea2c8fd1117f0b1c8261408ab16aae4eedf6e4
maccree <maccree.a@yandex.ru>
2019-08-11 20:41:00 +0300

    commands

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import token_for_bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 updater = Updater(token=token_for_bot) # token API  Telegram
 dispatcher = updater.dispatche
+
+
+# comends ob
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Hi')
+def textMessage(bot, update):
+    response = 'Ur message: ' + update.message.text
+    bot.send_message(chat_id=update.message.chat_id, text=response)
