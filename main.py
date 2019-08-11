6a636566c41af578c6dd54a68ed7d924937ecf28
maccree <maccree.a@yandex.ru>
2019-08-11 20:40:31 +0300

    doSET

---
+++ main.py
@@ -0,0 +1,5 @@
+#imports and sertting
+import token_for_bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+updater = Updater(token=token_for_bot) # token API  Telegram
+dispatcher = updater.dispatche
