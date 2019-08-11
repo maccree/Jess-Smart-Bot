601502578db5e12fd62c770c84b3a4f18622e632
maccree <maccree.a@yandex.ru>
2019-08-11 23:27:57 +0300

    add apiai and json

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #imports and sertting
 import token_for_bot
+import json
+import apiai
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 updater = Updater(token=token_for_bot) # token API  Telegram
 dispatcher = updater.dispatche
