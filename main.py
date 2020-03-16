38c285e999c270c7407028338bc4c276ddb5ee90
maccree <maccree.a@yandex.ru>
2020-03-16 23:14:47 +0300

    Working engine on the latest version of " telegram-python-bat"

--- main.py
+++ main.py
@@ -1,45 +1,33 @@
-#imports and sertting
-import token_for_bot
-import token_for_dialogflow
-import json
-import apiai
+#Settings
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import apiai, json
 
 
+updater = Updater(token='YOUT TOKEN HERE')      # API token to Telegram
+dispatcher = updater.dispatcher
 
-updater = Updater('<ur telegram token>', use_context= True) # token API  Telegram
-dispatcher = updater.dispatche
-
-
-# comends ob
+# Command processing
 def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Hi')
-
+    bot.send_message(chat_id=update.message.chat_id, text='Hello!')
 def textMessage(bot, update):
-
-    request = apiai.ApiAI('<ur Dialogflow Token>').text_request()                      # Dialogflow API Token
-
-    request.lang = 'ru'                                                             # In what language will the request be sent
-    request.session_id = 'BatlabAIBot'                                              # Dialog Session ID (you need to learn the bot later)
-    request.query = update.message.text                                             # We send a request to the "AI" with a message from the user
+    request = apiai.ApiAI('YOUR TOKEN HERE').text_request()
+    request.lang = 'ru' # What language will the request be sent in
+    request.session_id = 'BatlabAIBot'  # ID of the dialog Session (you need to teach the bot later)
+    request.query = update.message.text     # Sending a request to the AI with a message from the user
     responseJson = json.loads(request.getresponse().read().decode('utf-8'))
-    response = responseJson['result']['fulfillment']['speech']                      # Parse JSON and pull out the answer
-                                                                                    # If there is an answer from the bot - send it to the user, if not - the bot did not understand it
+    response = responseJson['result']['fulfillment']['speech']      # Parsing the JSON and pulling out the response
+        # If there is a response from the bot-send it to the user, if not-the bot did not understand it
     if response:
         bot.send_message(chat_id=update.message.chat_id, text=response)
     else:
-        bot.send_message(chat_id=update.message.chat_id, text='I do not quite understand you!')
+        bot.send_message(chat_id=update.message.chat_id, text='idnk')
 
-    #Handlers
+        # Handlers
 start_command_handler = CommandHandler('start', startCommand)
 text_message_handler = MessageHandler(Filters.text, textMessage)
-
-
-
-# HAndlers on Disp
+# Adding handlers to the dispatcher
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
-
-
+# Starting the search for updates
 updater.start_polling(clean=True)
 updater.idle()
